c2a71162ecd52410ff81966fce32b31a39196f57
Rocio <163675602+rodoher@users.noreply.github.com>
2024-05-20 17:57:52 +0200

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -1,26 +1,42 @@
 import socket
 import common_ports
-import pandas as pd
 
 def get_open_ports(target, port_range, verbose = False):
-    open_ports = []
+    if target[0].isdigit():
+        try:
+            socket.inet_aton(target)
 
-    for i in range(port_range[0], port_range[1]):
+        except socket.error:
+            return("Error: Invalid IP address")
+
+    else:
+        try:
+            socket.gethostbyname(target)
+        except:
+            return("Error: Invalid hostname")
+
+
+    open_ports = []
+    for i in range(port_range[0], port_range[1]+1):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(5)
+        s.settimeout(1)
         if s.connect_ex((target, i)):
             continue
 
         else:
             open_ports.append(i)
+    try:
+        ipnum = socket.gethostbyname(target)
+        print("open ports for "+target+" ("+ipnum+")")
+    except:
+        hostname = socket.gethostbyaddr(target)
+        print("open ports for "+hostname[0]+" ("+target+")")
 
     if verbose==True:
-        dic_ports = {}
+        text = "PORT     SERVICE\n"
         for i in open_ports:
             val = common_ports.ports_and_services[i]
-            dic_ports[i] = val
-        df = pd.DataFrame.from_dict(dic_ports, orient="index")
-        df = df.reset_index()
-        df.columns = ['PORT', 'SERVICE']
-        return df
+            text += str(i)+"       "+val+"\n"
+
+        return (text)
     return(open_ports)
